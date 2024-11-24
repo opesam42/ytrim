d35adbafb95470b5a9954953a946d14ae4fafa79
Gbenga Opeyemi <opesam42@gmail.com>
2024-11-24 23:04:05 +0100

    modified utils.py

--- main/tests.py
+++ main/tests.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Create your tests here.
-cookies = os.path.join(settings.MEDIA_ROOT, "ytcookies.txt")
+# cookies = os.path.join(settings.MEDIA_ROOT, "ytcookies.txt")
+cookies = "https://filesamples.com/samples/document/txt/sample3.txt"
 f = open(cookies, 'r')
 
 print(f.read())
