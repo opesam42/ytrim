00b16f63d56deaf53f6af5f7996a1cd74b3e76ed
Gbenga Opeyemi <opesam42@gmail.com>
2024-11-26 22:34:07 +0100

    import os to selenium py

--- main/selenium.py
+++ main/selenium.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import os
 
 
 def fetchPageWithHeaders(url):
