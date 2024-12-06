0ef4b7f87927480b83216bdf7019bc638f7f16c4
Gbenga Opeyemi <opesam42@gmail.com>
2024-12-06 20:55:02 +0100

    update build sh to setup pip

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-# build.sh
+#!/bin/bash
 echo "Starting build script"
 
-pip install -r requirements.txt
+# Use Python 3 to ensure pip is found
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # collectstatic
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 
 echo "Build script completed"
