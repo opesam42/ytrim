a62ca4401ef8ffe2ebb00ccb516a3e825a146b65
Gbenga Opeyemi <opesam42@gmail.com>
2024-12-06 22:28:17 +0100

    added venv to build

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 echo "Starting build script"
 
+python3 -m venv venv
+source venv/bin/activate
+
+echo "setup virtural env"
+
 # Use Python 3 to ensure pip is found
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
 # collectstatic
 python3 manage.py collectstatic --noinput
+
+#deactivate virural environment
+deactivate
 
 echo "Build script completed"
