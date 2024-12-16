1cb9647d6d3490bfeae813adcff06021fe362e09
Gbenga Opeyemi <opesam42@gmail.com>
2024-12-16 01:57:54 +0100

    add moviepy import *

--- main/utils1.py
+++ main/utils1.py
@@ -1,6 +1,7 @@
 import os
 from django.conf import settings
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy import *
+# from moviepy.video.io.VideoFileClip import VideoFileClip
 import yt_dlp
 from main.utilities.getproxy import test_proxy
 from .misc import Misc
